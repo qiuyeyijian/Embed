fcfb509aaea7fa44ed4e02386a3105807e9b117c
qiuyeyijian <qiuyeyijians@gmail.com>
2020-09-10 12:34:19 +0800

    添加第一个工程文件，添加README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Embed
 嵌入式实验工程文件
+
+### 配套教材
+
+嵌入式技术基础与实践（第4版）——ARM Cortex-M0+ KL系列微控制器
+
+作者：王宜怀 吴瑾 文瑾
+
+### 实验环境
+
+* AHL-GEC-IDE V4.11 （金葫芦GEC集成开发环境）
+* Kinetis Design Studio V3.2.0
+* TTL-USB串口(CH340)驱动
+* TTL-USB串口(PL2303)驱动
+
+[百度网盘](https://pan.baidu.com/s/1AcXlhTebK3HiLZZ_BLl3zw )    提取码：ivga
+
+
+
---
+++ src/红灯2秒闪烁一次/GPIO-Output-DirectAddress_KL36_20200907/Debug/obj/flash.d
@@ -0,0 +1,37 @@
+obj/flash.o: srcc/flash.c \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU\MCU_drivers/flash.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU/mcu.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU\startup/MKL36Z4.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cm0plus.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cmInstr.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cmFunc.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU\startup/system_MKL36Z4.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU\startup/system_MKL36Z4.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/cpu.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cm0plus.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cmFunc.h \
+ F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cmInstr.h
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU\MCU_drivers/flash.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU/mcu.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU\startup/MKL36Z4.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cm0plus.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cmInstr.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cmFunc.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU\startup/system_MKL36Z4.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\03_MCU\startup/system_MKL36Z4.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/cpu.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cm0plus.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cmFunc.h:
+
+F:\WorkSpace\AHL-GEC-EXP\04-Soft\CH04-GPIO\GPIO-Output-DirectAddress_KL36_20200907\02_CPU/core_cmInstr.h:
